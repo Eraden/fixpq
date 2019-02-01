1ba3dd6efc54affc97da8ce766531ead43bdf626
Adrian Wozniak <adrian.wozniak@ita-prog.pl>
2019-02-01 08:36:04 +0100

    Add lexer/parser

--- include/types.h
+++ include/types.h
@@ -1,15 +1,40 @@
-typedef enum Flag_e {
+#pragma once
+
+typedef enum {
     FLAG_NoOp,
     FLAG_Input,
     FLAG_Output,
     FLAG_Help
 } Flag;
 
-typedef struct State_t {
+typedef struct {
     char *input;
     char *output;
     enum Flag_e flag;
     FILE *in;
     FILE *out;
 } State;
+
+typedef enum {
+    TokenizerType_Keyword,
+    TokenizerType_Identifier,
+    TokenizerType_Operator,
+} TokenizerType;
+
+typedef enum {
+    LexerType_Keyword,
+    LexerType_Identifier,
+    LexerType_Operator,
+} LexerType;
+
+typedef struct {
+    TokenizerType type;
+    char *str;
+} TokenizerToken;
+
+typedef struct {
+    LexerToken *left;
+    LexerToken *right;
+    LexerType type;
+} LexerToken;
 
